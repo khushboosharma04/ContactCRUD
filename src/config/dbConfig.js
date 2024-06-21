3288f353da863102d7881d0ff4bcc8b48cd7af50
khushboosharma04 <s4khushboo@gmail.com>
2024-06-21 09:38:38 +0530

    added mysql config

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,13 +1,40 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const mysql = require("mysql");
 
 const connectDb = async () => {
-  try {
-    const connect = await mongoose.connect(process.env.MONGO_URL);
-    console.log("Database connected successfully!!");
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
+  const connection = mysql.createConnection({
+    host: "database-1.cxw40ieoe5ws.ap-south-1.rds.amazonaws.com",
+    user: "admin",
+    password: "mysql123",
+    database: "database1",
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to the database:", err.stack);
+      return;
+    }
+    console.log("Connected to the database.");
+  });
 };
 module.exports = connectDb;
+
+// const connectDb = async () => {
+//   try {
+//     const connect = await mongoose.connect(process.env.MONGO_URL);
+//     console.log("Database connected successfully!!");
+//   } catch (err) {
+//     console.log(err);
+//     process.exit(1);
+//   }
+// };
+// module.exports = connectDb;
+
+// connection.connect((err) => {
+//   if (err) {
+//     console.error("Error connecting to the database:", err.stack);
+//     return;
+//   }
+//   console.log("Connected to the database.");
+// });
